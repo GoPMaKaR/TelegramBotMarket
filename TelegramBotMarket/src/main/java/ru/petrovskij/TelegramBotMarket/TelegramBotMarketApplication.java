c60d1e3dd194619233cc691fc71b6586e43ab267
Александр <petrovskij_200111@mail.ru>
2023-06-24 12:54:48 +0300

    Initial commit

---
+++ TelegramBotMarket/src/main/java/ru/petrovskij/TelegramBotMarket/TelegramBotMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.petrovskij.TelegramBotMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotMarketApplication.class, args);
+	}
+
+}
---
+++ TelegramBotMarket/src/test/java/ru/petrovskij/TelegramBotMarket/TelegramBotMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrovskij.TelegramBotMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
